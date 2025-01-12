34ced14b202875cec055823146d60f1fbbb4941a
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-12 05:30:22 -0300

    frontend: Redux toolkit integration

--- frontend/src/app/DashboardWrapper.tsx
+++ frontend/src/app/DashboardWrapper.tsx
@@ -1,12 +1,30 @@
+"use client";
+
 import Navbar from '@/app/(components)/Navbar';
 import Sidebar from '@/app/(components)/Sidebar';
+import StoreProvider, { useAppSelector } from './redux';
+import { useEffect } from 'react';
 
-const DashboardWrapper = ({ children }: {children: React.ReactNode }) => {
+
+const DashboardLayout = ({ children }: {children: React.ReactNode }) => {
+
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed );
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode );
+
+  useEffect( () => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (<>
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
         {/* sidebar */}
         <Sidebar />
-        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? "":"md:pl-64"}`}>
             {/* navbar */}
             <Navbar />
             {children}
@@ -15,5 +33,16 @@ const DashboardWrapper = ({ children }: {children: React.ReactNode }) => {
     </>
   )
 }
+
+const DashboardWrapper = ({ children }: {children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>
+        {children}
+      </DashboardLayout>
+    </StoreProvider>
+  )
+}
+
 
 export default DashboardWrapper
